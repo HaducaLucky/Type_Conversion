1fa480388e6182afd5858792d81dd5f2402b4d12
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-18 18:59:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// type conversion = change the datatypes of a value to another
+//                   (strings, numbers, booleans)
+
+/* let age = window.prompt("How old are you?");
+age = Number(age);
+age+=1;
+
+console.log(age, typeof age); */
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
